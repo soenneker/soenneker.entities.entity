e0fdb1472eca6d5c26c4788f3f36460a8963d3b0
Jake Soenneker <jake@soenneker.com>
2025-08-16 13:06:22 -0500

    no longer abstract

--- src/Entity.cs
+++ src/Entity.cs
@@ -6,17 +6,17 @@ using Soenneker.Entities.Entity.Abstract;
 namespace Soenneker.Entities.Entity;
 
 /// <inheritdoc cref="IEntity"/>
-public abstract class Entity : IEntity
+public class Entity : IEntity
 {
     [JsonPropertyName("id")]
     [JsonProperty("id")]
-    public virtual string Id { get; set; } = default!;
+    public virtual string Id { get; set; } = null!;
 
     [JsonPropertyName("createdAt")]
     [JsonProperty("createdAt")]
-    public DateTime CreatedAt { get; set; }
+    public virtual DateTime CreatedAt { get; set; }
 
     [JsonPropertyName("modifiedAt")]
     [JsonProperty("modifiedAt")]
-    public DateTime? ModifiedAt { get; set; }
+    public virtual DateTime? ModifiedAt { get; set; }
 }
